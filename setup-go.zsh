adde7a27ce901eec07fce8d80f421a9a128f1f41
snowyowl-bitbucket-admin <bennyyjacob@gmail.com>
2021-02-06 13:58:30 +0530

    Adding install script for go

---
+++ setup-go.zsh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+GO_PKG="go1.15.8.linux-amd64.tar.gz" 
+
+wget https://golang.org/dl/"{GO_PKG}" && tar -C /usr/local -xzf "{GO_PKG}" && echo "export PATH=${PATH}:/usr/local/go/bin">~/.oh-my-zsh/custom/go_path.zsh
+
