a7fa009c48c81b04b1c442a4a48a3c6bad88ad6f
snowyowl-bitbucket-admin <bennyyjacob@gmail.com>
2021-02-06 14:22:19 +0530

    Adding JS recon script

---
+++ setup-js-recon.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+echo "Installing gau"
+GO111MODULE=on go get -u -v github.com/lc/gau
+
+echo "Installing fuff"
+go get -u github.com/ffuf/ffuf
+
+echo "Installng hakrawler"
+go get github.com/hakluke/hakrawler
+
+echo "Installing httpx"
+GO111MODULE=on go get -v github.com/projectdiscovery/httpx/cmd/httpx
+
+echo "Installing unfurl"
+go get -u github.com/tomnomnom/unfurl
+
+# debug info to ensure that all the tools are in-place
+go env GOPATH | xargs -I % ls -alt %/bin
+
+echo "Installing scripthunter"
+DIR="~/workspace/tools" && mkdir -p "${DIR}" && cd "${DIR}" &&  git clone https://github.com/robre/scripthunter.git && cd "${DIR}"/scripthunter
+
+echo "Done"
+
