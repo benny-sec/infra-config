395b3142304c75cbc5ccd42834a9a39a40b4a628
benny-sec <68265895+benny-sec@users.noreply.github.com>
2021-02-06 16:08:16 +0530

    Update setup-go.zsh

--- setup-go.zsh
+++ setup-go.zsh
@@ -2,5 +2,5 @@
 
 GO_PKG="go1.15.8.linux-amd64.tar.gz" 
 
-wget https://golang.org/dl/"${GO_PKG}" && tar -C /usr/local -xzf "${GO_PKG}" && echo "export PATH=${PATH}:/usr/local/go/bin">~/.oh-my-zsh/custom/go_path.zsh
+wget https://golang.org/dl/"${GO_PKG}" && sudo tar -C /usr/local -xzf "${GO_PKG}" && echo "export PATH=${PATH}:/usr/local/go/bin">~/.oh-my-zsh/custom/go_path.zsh
 
