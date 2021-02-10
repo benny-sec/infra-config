a1d41051569b0ac0c2f5d0abc1d32e554c689729
benny-sec <68265895+benny-sec@users.noreply.github.com>
2021-02-10 20:32:30 +0530

    Update setup-postgres-for-atlassian.sh

--- setup-postgres-for-atlassian.sh
+++ setup-postgres-for-atlassian.sh
@@ -6,7 +6,7 @@ sudo apt-get update && sudo apt-get -y install postgresql-12
 
 sudo -s -H -u postgres
 # Create the Bamboo user:
-/opt/PostgreSQL/8.3/bin/createuser -S -d -r -P -E bamboouser
+createuser -S -d -r -P -E bamboouser
 # Create the bamboo database:
-/opt/PostgreSQL/8.3/bin/createdb -O bamboouser bamboo
+createdb -O bamboouser bamboo
 exit
