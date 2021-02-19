a47084d469ddf4c75322de6c15680e621f3a3d22
snowyowl-bitbucket-admin <bennyyjacob@gmail.com>
2021-02-19 09:53:53 +0530

    Adding jira setup files

---
+++ atlassian/jira/setup-jira.sh
@@ -0,0 +1,14 @@
+#/usr/bin/env bash
+
+JIRA_PKG="atlassian-jira-software-8.15.0-x64.bin"
+
+sudo apt -y install wget
+
+cd /tmp && wget https://product-downloads.atlassian.com/software/jira/downloads/"${JIRA_PKG}"
+
+wget "https://raw.githubusercontent.com/benny-sec/infra-config/main/jira/response.varfile"
+
+chmod +x "${JIRA_PKG}"
+
+sudo ./"${JIRA_PKG}" -q -varfile response.varfile
+
