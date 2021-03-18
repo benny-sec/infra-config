b046c2ee65600ee626d6b3eeea016ca238a2dbce
snowyowl-bitbucket-admin <bennyyjacob@gmail.com>
2021-03-18 14:00:14 +0530

    Updating terraform config 1. Removed the second compute instance 2. Removed the static ip.

--- iac/gcp/base/base.tf
+++ iac/gcp/base/base.tf
@@ -20,12 +20,8 @@ data "http" "base-infra-script" {
   url = "https://raw.githubusercontent.com/benny-sec/infra-config/main/basic-infra-config.sh"
 }
 
-resource "google_compute_address" "vm_static_ip" {
-  name = "rev-shell-static-ip"
-}
-
-resource "google_compute_instance" "bamboo_instance" {
-  name         = "bamboo-server"
+resource "google_compute_instance" "base_instance" {
+  name         = "base-server"
   machine_type = "e2-standard-2"
 
   boot_disk {
@@ -49,7 +45,7 @@ resource "google_compute_instance" "bamboo_instance" {
         type        = "ssh"
         user        = "blackhawk"
         private_key = file("/home/dare/workspace/creds/google-cloud-def-key")
-        host        = google_compute_instance.bamboo_instance.network_interface[0].access_config[0].nat_ip
+        host        = google_compute_instance.base_instance.network_interface[0].access_config[0].nat_ip
     } 
 
 
@@ -60,7 +56,7 @@ resource "google_compute_instance" "bamboo_instance" {
         type        = "ssh"
         user        = "blackhawk"
         private_key = file("/home/dare/workspace/creds/google-cloud-def-key")
-        host        = google_compute_instance.bamboo_instance.network_interface[0].access_config[0].nat_ip
+        host        = google_compute_instance.base_instance.network_interface[0].access_config[0].nat_ip
     } 
     
     inline = [
@@ -70,29 +66,8 @@ resource "google_compute_instance" "bamboo_instance" {
   }
 }
 
-resource "google_compute_instance" "rev_shell_instance" {
-  name         = "rev-shell"
-  machine_type = "f1-micro"
-
-  boot_disk {
-    initialize_params {
-      image = "ubuntu-os-cloud/ubuntu-2004-focal-v20210211"
-    }
-  }
-
-  network_interface {
-    network = "default"
-    access_config {
-      nat_ip = google_compute_address.vm_static_ip.address
-    }
-  }
-}
-
-output "static_ip_revshell" {
-    value =  google_compute_address.vm_static_ip.address
-}
-output "ip_bamboo_server" {
-    value =  google_compute_instance.bamboo_instance.network_interface[0].access_config[0].nat_ip
+output "ip_base_server" {
+    value =  google_compute_instance.base_instance.network_interface[0].access_config[0].nat_ip
 }
 
 
