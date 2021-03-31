02f8b619534947fe65384e648a4159463ce7d752
benny-sec <68265895+benny-sec@users.noreply.github.com>
2021-03-31 12:00:09 +0530

    Update setup-jira.sh

--- atlassian/jira/setup-jira.sh
+++ atlassian/jira/setup-jira.sh
@@ -4,6 +4,9 @@ JIRA_PKG="atlassian-jira-software-8.16.0-x64.bin"
 
 sudo apt -y install wget fontconfig
 
+# Install PostgreSQL if not already installed
+which psql > /dev/null || bash -c "$(wget -O- https://raw.githubusercontent.com/benny-sec/infra-config/main/setup-postgres-for-atlassian.sh)"
+
 cd /tmp && wget https://product-downloads.atlassian.com/software/jira/downloads/"${JIRA_PKG}"
 
 wget "https://raw.githubusercontent.com/benny-sec/infra-config/main/atlassian/jira/response.varfile"
