c623f6c50ae6abde3c2b711dfb93b9b271495f6d
benny-sec <68265895+benny-sec@users.noreply.github.com>
2021-05-02 13:25:59 +0530

    Update basic-infra-config.sh

--- basic-infra-config.sh
+++ basic-infra-config.sh
@@ -2,7 +2,7 @@
 
 mkdir -p ~/workspace/tools 
 
-sudo apt-get update && sudo apt -y install git zsh neovim net-tools wget
+sudo apt-get update && sudo apt -y install git zsh neovim net-tools wget tree
 
 # install oh-my-zsh and change the shell to zsh for the user blackhawk
 cd /tmp && wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh && sh install.sh --unattended && sudo chsh --shell /bin/zsh $USER
@@ -24,3 +24,7 @@ echo -e "alias vim=nvim\nalias ll='ls -alt'">~/.oh-my-zsh/custom/aliases.zsh
 
 # fix locales
 sudo locale-gen en_US && sudo locale-gen en_US.UTF-8 && export LC_ALL="en_US.UTF-8" && sudo update-locale LC_ALL=en_US.UTF-8 && . /etc/default/locale
+
+# Clone the infra-config repo and dump the commands for a few quick set-up (e.g Atlassian servers )
+git clone https://github.com/benny-sec/infra-config.git ~/workspace/tools/infra-config
+cat ~/workspace/tools/infra-config/README.md
