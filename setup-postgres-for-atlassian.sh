418ce4e7c2b73257f9ff5e7f44e08cd517a3ea50
benny-sec <68265895+benny-sec@users.noreply.github.com>
2021-05-26 20:37:21 +0530

    Update setup-postgres-for-atlassian.sh

--- setup-postgres-for-atlassian.sh
+++ setup-postgres-for-atlassian.sh
@@ -1,34 +1,110 @@
 #!/usr/bin/env bash
 
-sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
-wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
-# postgresql for older versions of atlassian products (e.g Confluence 5.6.5)
-# sudo apt-get update && sudo apt-get -y install postgresql-9.3
-sudo apt-get update && sudo apt-get -y install postgresql-11
+echo_color() {
+    case $1 in
+        "red")
+            echo "`tput setaf 1``tput bold`$2`tput sgr0`"
+            ;;
+        *)
+            echo "`tput setaf 4``tput bold`$1`tput sgr0`"
+            ;;
+    esac
+}
 
-# Create the Jira user and a DB:
-sudo -u postgres psql -c "CREATE ROLE jirauser PASSWORD 'pass' NOSUPERUSER CREATEDB CREATEROLE INHERIT LOGIN;"
-sudo -u postgres psql -c "CREATE DATABASE jiradb OWNER jirauser;"
+get_args() {
+    while [[ $# -gt 0 ]]
+    do
+        key="$1"
+        case $key in
+            "--install-version")
+                POSTGRES_VERSION="$2"
+                shift # past argument
+                shift # past value
+                ;;
+            --help|-h)
+                print_usage
+                exit 0
+                ;;
+            *)    # unknown option
+                shift # past argument
+                ;;
+        esac
+    done
+}
 
-# Create the Confluence user and a DB:
-sudo -u postgres psql -c "CREATE ROLE confluenceuser PASSWORD 'pass' NOSUPERUSER CREATEDB CREATEROLE INHERIT LOGIN;"
-sudo -u postgres psql -c "CREATE DATABASE confluencedb OWNER confluenceuser;"
+set_install_params() {
+    case $POSTGRES_VERSION in
+        "9.3") # Java SE 1.7
+            POSTGRES_VERSION="postgresql-9.3"
+            ;;
+        *)    # unknown option
+            echo_color "red" "Defaulting to version postgresql-11"
+            POSTGRES_VERSION="postgresql-11"
+            ;;
+    esac
+}
 
-# Create the Bitbucket user and a DB:
-sudo -u postgres psql -c "CREATE ROLE bitbucketuser PASSWORD 'pass' NOSUPERUSER CREATEDB CREATEROLE INHERIT LOGIN;"
-sudo -u postgres psql -c "CREATE DATABASE bitbucketdb OWNER bitbucketuser;"
+install_postgresql() {
 
-# Create the Bamboo user and a DB:
-sudo -u postgres psql -c "CREATE ROLE bamboouser PASSWORD 'pass' NOSUPERUSER CREATEDB CREATEROLE INHERIT LOGIN;"
-sudo -u postgres psql -c "CREATE DATABASE bamboodb OWNER bamboouser;"
+    sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+    wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+    sudo apt-get update && sudo apt-get -y install ${POSTGRES_VERSION}
 
-# Create the Fisheye user and a DB:
-sudo -u postgres psql -c "CREATE ROLE fisheyeuser PASSWORD 'pass' NOSUPERUSER CREATEDB CREATEROLE INHERIT LOGIN;"
-sudo -u postgres psql -c "CREATE DATABASE fisheyedb OWNER fisheyeuser;"
+}
 
-# Create the Crowd user and a DB:
-sudo -u postgres psql -c "CREATE ROLE crowduser PASSWORD 'pass' NOSUPERUSER CREATEDB CREATEROLE INHERIT LOGIN;"
-sudo -u postgres psql -c "CREATE DATABASE crowddb OWNER crowduser;"
+create_db() {
+    # Create the Jira user and a DB:
+    sudo -u postgres psql -c "CREATE ROLE jirauser PASSWORD 'pass' NOSUPERUSER CREATEDB CREATEROLE INHERIT LOGIN;"
+    sudo -u postgres psql -c "CREATE DATABASE jiradb OWNER jirauser;"
 
+    # Create the Confluence user and a DB:
+    sudo -u postgres psql -c "CREATE ROLE confluenceuser PASSWORD 'pass' NOSUPERUSER CREATEDB CREATEROLE INHERIT LOGIN;"
+    sudo -u postgres psql -c "CREATE DATABASE confluencedb OWNER confluenceuser;"
 
+    # Create the Bitbucket user and a DB:
+    sudo -u postgres psql -c "CREATE ROLE bitbucketuser PASSWORD 'pass' NOSUPERUSER CREATEDB CREATEROLE INHERIT LOGIN;"
+    sudo -u postgres psql -c "CREATE DATABASE bitbucketdb OWNER bitbucketuser;"
 
+    # Create the Bamboo user and a DB:
+    sudo -u postgres psql -c "CREATE ROLE bamboouser PASSWORD 'pass' NOSUPERUSER CREATEDB CREATEROLE INHERIT LOGIN;"
+    sudo -u postgres psql -c "CREATE DATABASE bamboodb OWNER bamboouser;"
+
+    # Create the Fisheye user and a DB:
+    sudo -u postgres psql -c "CREATE ROLE fisheyeuser PASSWORD 'pass' NOSUPERUSER CREATEDB CREATEROLE INHERIT LOGIN;"
+    sudo -u postgres psql -c "CREATE DATABASE fisheyedb OWNER fisheyeuser;"
+
+    # Create the Crowd user and a DB:
+    sudo -u postgres psql -c "CREATE ROLE crowduser PASSWORD 'pass' NOSUPERUSER CREATEDB CREATEROLE INHERIT LOGIN;"
+    sudo -u postgres psql -c "CREATE DATABASE crowddb OWNER crowduser;"
+}
+
+print_usage()
+{
+    cat <<-EOF
+
+    setup-postgres-for-atlassian.sh [--install-version 9.3]
+
+    The script will install PostgreSQL 11 (by default) and creates the DB for atlassian products
+    If the optional --install-version is specified the specified version is attempted.
+
+    --install-version 9.3
+        The PostgreSQL version to install.
+
+    -h prints this text
+EOF
+}
+
+main() {
+
+    get_args "$@"
+    set_install_params
+
+    echo_color "Installing ${POSTGRES_VERSION} "
+
+    install_postgresql
+    create_db
+
+    echo_color "Finished Installing ${POSTGRES_VERSION}"
+}
+
+main "$@"
