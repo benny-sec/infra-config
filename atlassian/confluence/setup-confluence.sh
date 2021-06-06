42eac4aadbf2c9397da8f06d5d9b3cb3fab3021b
benny-sec <68265895+benny-sec@users.noreply.github.com>
2021-06-06 16:46:26 +0530

    Update setup-confluence.sh

--- atlassian/confluence/setup-confluence.sh
+++ atlassian/confluence/setup-confluence.sh
@@ -4,6 +4,12 @@ CONFLUENCE_PKG="atlassian-confluence-7.12.0-x64.bin"
 
 sudo apt -y install wget fontconfig
 
+cd /tmp
+
+# PostgreSQL for Older versions of confluence.
+#wget https://raw.githubusercontent.com/benny-sec/infra-config/main/setup-postgres-for-atlassian.sh
+#chmod +x setup-postgres-for-atlassian.sh && ./setup-postgres-for-atlassian.sh --install-version 9.3
+
 # Install PostgreSQL if not already installed
 which psql > /dev/null || bash -c "$(wget -O- https://raw.githubusercontent.com/benny-sec/infra-config/main/setup-postgres-for-atlassian.sh)"
 
