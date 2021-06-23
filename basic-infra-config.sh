c416ac777260e25ae5053b7e421963ff1e1fcb9e
benny-sec <68265895+benny-sec@users.noreply.github.com>
2021-06-23 14:08:01 +0530

    Add tmux support
    Perform basic configuration for vi-mode, clipboard, mouse support and increasing history limit to 100K

--- basic-infra-config.sh
+++ basic-infra-config.sh
@@ -2,7 +2,7 @@
 
 mkdir -p ~/workspace/tools 
 
-sudo apt-get update && sudo apt -y install git zsh neovim net-tools wget tree x11-apps
+sudo apt-get update && sudo apt -y install git zsh neovim net-tools wget tree x11-apps xsel
 
 # install oh-my-zsh and change the shell to zsh for the user blackhawk
 cd /tmp && wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh && sh install.sh --unattended && sudo chsh --shell /bin/zsh $USER
@@ -28,3 +28,16 @@ sudo locale-gen en_US && sudo locale-gen en_US.UTF-8 && export LC_ALL="en_US.UTF
 # Clone the infra-config repo and dump the commands for a few quick set-up (e.g Atlassian servers )
 git clone https://github.com/benny-sec/infra-config.git ~/workspace/tools/infra-config
 cat ~/workspace/tools/infra-config/README.md
+
+# set-up tmux
+cd
+git clone https://github.com/gpakosz/.tmux.git
+ln -s -f .tmux/.tmux.conf
+cp .tmux/.tmux.conf.local .
+sed -i s/"#set -g mode-keys vi"/"set -g mode-keys vi"/g .tmux.conf.local
+sed -i s/"#set -g history-limit 10000"/"set -g history-limit 100000"/g .tmux.conf.local
+sed -i s/"tmux_conf_copy_to_os_clipboard=false"/"tmux_conf_copy_to_os_clipboard=true"/g .tmux.conf.local
+sed -i s/"#set -g mouse on"/"set -g mouse on"/g .tmux.conf.local
+
+
+
