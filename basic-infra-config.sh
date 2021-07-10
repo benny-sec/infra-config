ccdf8f06e96092f19e8de3575f72378463f21901
benny-sec <68265895+benny-sec@users.noreply.github.com>
2021-07-10 20:58:28 +0530

    Update basic-infra-config.sh

--- basic-infra-config.sh
+++ basic-infra-config.sh
@@ -48,6 +48,13 @@ alias sudo='sudo '
 # System wide nvim configuration is to be identified. For now copying the settings to root
 sudo mkdir -p /root/.config/nvim && sudo cp /home/blackhawk/.config/nvim/init.vim /root/.config/nvim/
 
+# install vim-plug; ideally the default packager manager in neovim should suffice
+# but for now couldn't get fzf.vim working with the default package manager
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
 # mapping of menu key to backspace and Scroll lock to Casps Lock
-#xmodmap -e "keycode 135 = BackSpace" -e "keycode 78 = Caps_Lock"
+# xmodmap -e "keycode 135 = BackSpace" -e "keycode 78 = Caps_Lock"
+
+
 
